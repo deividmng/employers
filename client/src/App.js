9779ab3abaed6990b8b31ca3aed2a38d34553f4b
deividmng <mangassk8@gmail.com>
2025-01-12 10:13:00 +0000

    layout and crud nearly done

---
+++ client/src/App.js
@@ -0,0 +1,199 @@
+import './App.css';
+import { useState } from "react";
+import  Axios  from "axios";
+
+
+function App() {
+  const [nombre, setNombre] = useState("");
+  const [edad, setEdad] = useState();
+  const [pais, setPais] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [year, setYear] = useState();
+  const [id, setId] = useState();
+
+  /// this is the button to edit 
+  const[editar,setEditar] = useState(false)
+
+  const[empleadosList,setEmpleados] = useState([])
+
+
+
+  
+  const add = () => {
+    // Mostrar en consola los datos que se van a enviar
+    console.log(nombre, edad, pais, cargo, year);
+
+    Axios.post("http://localhost:3000/create", {
+      nombre: nombre,
+      edad: edad,
+      pais: pais,
+      cargo: cargo,
+      anios: year
+  })
+  .then(response => {
+    getEmpleados();
+
+      console.log("Empleado registrado:", response.data);
+ 
+
+  })
+  .catch(error => {
+      console.error("Error al registrar empleado:", error);
+  });
+  
+
+  };
+//* function the get the empeleados 
+  const getEmpleados = () => {
+    // Cambiar de POST a GET 
+    //! the error was on the localhost that it was in 3001 
+    Axios.get("http://localhost:3000/empleados")
+      .then((response) => {
+        setEmpleados(response.data); // in case in goes well 
+      })
+      .catch((error) => {
+        console.error("Error al obtener empleados:", error);
+      });
+  };
+  
+  getEmpleados();
+
+  //* function to addate
+  const editarEmpleado = (val) => {
+    setEditar(true);
+
+    setId(val.id);
+    setNombre(val.nombre);
+    setEdad(val.edad);
+    setPais(val.pais);
+    setCargo(val.cargo);
+    setYear(val.anios);
+    
+  }
+
+  
+  // const mostrarDatos = () => {
+  //   // here is the button, to add the people 
+  //     console.log(nombre,edad,pais,cargo,year)
+  //   }
+  return (
+    <div className="App">
+      <h1>Formulario</h1>
+      <div className="datos">
+        <label>
+          Nombre:
+              {/*with onChange it target each time it happen any change 
+                (event) it the atributo .value acces to the value 
+              */}
+          <input
+          value={nombre}
+            type="text"
+            onChange={(event) => {
+              setNombre(event.target.value);
+              
+            }}
+          />
+        </label>
+        <br />
+        <label>
+          Edad:
+          <input
+          value={edad}
+            type="number"
+            onChange={(event) => {
+              setEdad(Number(event.target.value));
+            }}
+          />
+        </label>
+        <br />
+        <label>
+          País:
+          <input
+          value={pais}
+            type="text"
+            onChange={(event) => {
+              setPais(event.target.value);
+            }}
+          />
+        </label>
+        <br />
+        <label>
+          Cargo:
+          <input
+          value={cargo}
+            type="text"
+            onChange={(event) => {
+              setCargo(event.target.value);
+            }}
+          />
+        </label>
+        <br />
+        <label>
+          Año:
+          <input
+            value={year}
+            type="number"
+            onChange={(event) => {
+              setYear(Number(event.target.value));
+            }}
+          />
+        </label>
+        <br />
+        <button onClick={add} >
+          Add
+        </button>
+      </div>
+      <div className='lista'>
+      {/* <button onClick={getEmpleados} >
+          Listar
+        </button> */}
+      </div>
+
+      <div className="listaDatos">
+  <table className="tablaEmpleados">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>Nombre</th>
+        <th>Edad</th>
+        <th>País</th>
+        <th>Cargo</th>
+        <th>Años</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        empleadosList.map((val, key) => (
+          <tr key={key}>
+            <td>{val.id}</td>
+            <td>{val.nombre}</td>
+            <td>{val.edad}</td>
+            <td>{val.pais}</td>
+            <td>{val.cargo}</td>
+            <td>{val.anios}</td>
+            <td>
+            <div className='buttosAct'>
+            <button className='edict' onClick={()=> {
+              editarEmpleado(val);
+            }} >
+          editar
+        </button>
+            <button className='delete' onClick={add} >
+          Add
+        </button>
+            </div>
+            </td>
+          </tr>
+        ))
+      }
+    </tbody>
+  </table>
+</div>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const cors = require("cors")
+// callin to listenning on port N 
+ app.use(cors());
+ app.use(express.json())
+
+// crating the connection at sql
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"empleados_crud"
+})
+
+// saving the data with the mettod post the root it  gonna call on create , req = solicitud , res = respond 
+app.post("/create", (req, res) => {
+    const { nombre, edad, pais, cargo, anios } = req.body;
+
+    const query = 'INSERT INTO empleados (nombre, edad, pais, cargo, anios) VALUES (?, ?, ?, ?, ?)';
+
+    db.query(query, [nombre, edad, pais, cargo, anios], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error al registrar el empleado");
+        } else {
+            res.send("Empleado registrado exitosamente");
+        }
+    });
+});
+
+//! to get the data fro the sql 
+app.get("/empleados", (req, res) => {
+    const query = 'SELECT * FROM empleados'; // Asegúrate de que la consulta esté bien
+    db.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error al obtener empleados");
+      } else {
+        res.send(result);
+      }
+    });
+  });
+  
+//! here it goes the actualizar 
+
+app.put("/update", (req, res) => {
+    const { id, nombre, edad, pais, cargo, anios } = req.body;
+  
+    //! worng way   const query = 'UPDATE empleados SET (id, nombre, edad, pais, cargo, anios) VALUES (?, ?, ?, ?, ?) WHERE id = ?';
+    const query = `UPDATE empleados 
+                   SET nombre = ?, edad = ?, pais = ?, cargo = ?, anios = ? 
+                   WHERE id = ?`;
+  
+    db.query(query, [nombre, edad, pais, cargo, anios, id], (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error al actualizar el empleado");
+      } else {
+        res.send("Empleado actualizado exitosamente");
+      }
+    });
+  });
+  
+
+
+/// to be sure is connected 
+app.post("/create", (req, res) => {
+    console.log("Ruta /create alcanzada");
+    // Resto del código
+});
+
+
+app.listen(3001, () => {
+    console.log("Server is listening on port 3000");
+});
+
+
